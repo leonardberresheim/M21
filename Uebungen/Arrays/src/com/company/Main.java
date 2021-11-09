49a1a6dcd81698d6202cdef53fca012e8b2ddb01
Leonard Berresheim <berreshe@htw-berlin.de>
2021-11-09 07:11:55 +0100

    Add uebungen arrays and schleifen verzweigungen

---
+++ Uebungen/Arrays/src/com/company/Main.java
@@ -0,0 +1,56 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // ------------------------------------------------------------------
+        // ------------------------- Aufgabe 1 ------------------------------
+        // Es soll ein Array initialisiert werden mit den Werten 0 bis 10
+
+
+        // Die Zahl an der 5ten Stelle soll mit der 32 ersetzt werden
+
+        // Die Zahl an der 5ten Stelle soll ausgegeben werden
+
+        // Die Zahl an der ersten Stelle soll mit der Zahl an der letzten Stelle ersetzt werden
+
+
+        // Die Zahl an der zweiten Stelle soll mit der zahl an der dritten Stelle getauscht werden.
+
+        // Geben Sie Stelle 2 und 3 aus.
+
+
+        // ------------------------------------------------------------------
+        // ------------------------- Aufgabe 2 ------------------------------
+        // Es soll ein Array mit allen Wochentagen definiert werden.
+
+
+
+
+
+        // Geben sie jeden Wochentag aus
+
+
+        // Wenn der Wochentag inerhalb der Woche ist soll Arbeiten!
+        // ausgegeben werden - Ansonsten Wochenende!
+
+
+
+        // ------------------------------------------------------------------
+        // ------------------------- Aufgabe 3 ------------------------------
+        // Es soll ein Array initialisiert werden mit den Werten 0 bis 10
+        //int[] zahlen = new int[10];
+
+
+        // Es soll die Summe dieser Zahlen berechnet werden
+
+
+        // ------------------------------------------------------------------
+        // ------------------------- Aufgabe 4 ------------------------------
+        // Selbe wie Aufgabe 3 nur mit einer vom Nutzer eingegebenen Zahl
+
+        // ------------------------------------------------------------------
+        // ------------------------- Aufgabe 5 ------------------------------
+        // Selbe wie Aufgabe 2 nur mit foreach
+    }
+}
---
+++ Uebungen/Schleifen_Verzweigungen/src/com/company/Main.java
@@ -0,0 +1,73 @@
+package com.company;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        //----------------------------------------------------------------
+        //----------------------- Aufgabe 1 ------------------------------
+        // Der Nutzer gibt 2 Zahlen ein - Es soll die Summe aller Zahlen
+        // zwichen diesen beiden Zahlen zusammen gerechnet werden
+        // Wenn die Summe jedoch den Wert 100 über steigt soll abgebrochen werden
+/*
+        int zahlA, zahlB, summe;
+        System.out.println("Geben Sie bitte 2 Zahlen ein");
+        System.out.print("Zahl A : ");
+        zahlA = scan.nextInt();
+        System.out.print("Zahl B : ");
+        zahlB = scan.nextInt();
+
+
+        System.out.println("Summe: " + summe);
+
+
+        //----------------------------------------------------------------
+        //----------------------- Aufgabe 2 ------------------------------
+        // Der Nutzer gibt eine Zahl ein und ein Zeichen
+        // Anschließend soll ein Dreieck aus dem Zeichen und in der Größe
+        // der Zahl abgebildet werden
+        // Beispiel: 5 - '+'
+        // +
+        // ++
+        // +++
+        // ++++
+        // +++++
+        int zahl;
+        char symbol;
+        System.out.print("Zahl: ");
+        zahl = scan.nextInt();
+        System.out.print("Symbol: ");
+        symbol = scan.next().charAt(0);
+
+
+        // Was muss verändert werden damit statt dreiecken Vierecke ausgegeben werden?
+        // Nun sollen mehrere Vierecke hintereinander ausgegeben werden nämlich genauso viele
+        // wie die Vierecke groß sind.
+
+
+        //----------------------------------------------------------------
+        //----------------------- Aufgabe 3 ------------------------------
+        // Es sollen zufällige Ziffern ausgegeben werden.
+        // Wenn die Ziffer $,?,# ist dann soll es einen Zeilenumbruch geben
+        // Wenn die Ziffer % ist dann soll das Programm beenden
+        Random rnd = new Random();
+        char c = ' ';
+        c = (char) (33 + rnd.nextInt(93));
+        
+ */
+
+
+
+
+
+
+
+
+
+
+        // Verändern sie das Programm so dass es midestens 10 Reihen ausgibt bevor es abbricht.
+
+    }
+}
